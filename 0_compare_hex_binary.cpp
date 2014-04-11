9d7d6b0cd08adb6e25b5503c3548b5362d4ec3f9
HybridRbt <littlejack0120@gmail.com>
2014-04-11 00:37:34 -0500

    added explicit type conversion

--- 0_compare_hex_binary.cpp
+++ 0_compare_hex_binary.cpp
@@ -17,12 +17,12 @@ public:
 	int convertToBase(string number, int base) {
 		if (base < 2 || (base > 10 && base != 16)) return -1;
 		int value = 0;
-		for (int i = number.length()-1; i >= 0; i--) {
+		for (int i = int(number.length()-1); i >= 0; i--) {
 			int digit = digitToValue(number[i]);
 			if (digit < 0 || digit >= base) {
 				return -1;
 			}
-			int exponent = number.length() - 1 - i;
+			int exponent = int(number.length()) - 1 - i;
 			value += digit * pow(base, exponent);
 		}
 		return value;
